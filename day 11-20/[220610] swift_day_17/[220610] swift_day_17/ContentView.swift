66c737f37cc3481f1cec887c2c50b893a63b7870
alpaka99 <ruvan123@naver.com>
2022-06-10 18:28:05 +0900

    [220610] swift day 17

---
+++ day 11-20/[220610] swift_day_17/[220610] swift_day_17/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  [220610] swift_day_17
+//
+//  Created by user on 2022/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+//        1. Reading text from the user with TextField
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    // 2. Creating Picker in a form
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+//                4. Adding segmented control for tup percentages
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+//                5. Calculationg the total per person
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+//            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day 11-20/[220610] swift_day_17/[220610] swift_day_17/_220610__swift_day_17App.swift
@@ -0,0 +1,17 @@
+//
+//  _220610__swift_day_17App.swift
+//  [220610] swift_day_17
+//
+//  Created by user on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct _220610__swift_day_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
