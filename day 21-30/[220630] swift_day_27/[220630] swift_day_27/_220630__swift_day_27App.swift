6cd38288b499fd718eb2f8ac8716d9a2c6b7f9d8
alpaka99 <ruvan123@naver.com>
2022-06-30 19:52:32 +0900

    [220630] swift_day_27

---
+++ day 21-30/[220630] swift_day_27/[220630] swift_day_27/_220630__swift_day_27App.swift
@@ -0,0 +1,17 @@
+//
+//  _220630__swift_day_27App.swift
+//  [220630] swift_day_27
+//
+//  Created by user on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct _220630__swift_day_27App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
