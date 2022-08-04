6f84fb92f3e56ca242407de543e2308933f2b520
alpaka99 <ruvan123@naver.com>
2022-08-04 23:21:01 +0900

    [220804] swift_day_43

---
+++ day 41-50/[220804] swift_day_43/[220804] swift_day_43/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  [220804] swift_day_43
+//
+//  Created by user on 2022/08/04.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    var insetAmount = 0.0
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+//        1. Creating custom paths with SwiftUI
+//        Path { path in
+//            path.move(to: CGPoint(x: 200, y: 100))
+//            path.addLine(to: CGPoint(x: 100, y: 300))
+//            path.addLine(to: CGPoint(x: 300, y: 300))
+//            path.addLine(to: CGPoint(x: 200, y: 100))
+//            path.closeSubpath()
+//        }
+//        .fill(.blue)
+//        .stroke(.blue,style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        
+//        2. Path vs Shapes in SwiftUI
+//        Triangle()
+//            .fill(.red)
+//            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//            .frame(width: 300, height: 300)
+        
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(.blue, lineWidth: 40)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day 41-50/[220804] swift_day_43/[220804] swift_day_43/_220804__swift_day_43App.swift
@@ -0,0 +1,17 @@
+//
+//  _220804__swift_day_43App.swift
+//  [220804] swift_day_43
+//
+//  Created by user on 2022/08/04.
+//
+
+import SwiftUI
+
+@main
+struct _220804__swift_day_43App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
