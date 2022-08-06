cb4356e2e44574e45b84abcb05761963f295a526
alpaka99 <ruvan123@naver.com>
2022-08-07 00:43:10 +0900

    [220806] swift_day_44

---
+++ day 41-50/[220806] swift_day_44/[220806] swift_day_44/_220806__swift_day_44App.swift
@@ -0,0 +1,17 @@
+//
+//  _220806__swift_day_44App.swift
+//  [220806] swift_day_44
+//
+//  Created by user on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct _220806__swift_day_44App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
