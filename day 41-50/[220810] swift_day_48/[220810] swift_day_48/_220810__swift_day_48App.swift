7991bfdcb0cebc2d971cb1a4ed4f438a3d9baa0e
alpaka99 <ruvan123@naver.com>
2022-08-11 02:09:00 +0900

    [220810] swift_day_48

---
+++ day 41-50/[220810] swift_day_48/[220810] swift_day_48/_220810__swift_day_48App.swift
@@ -0,0 +1,17 @@
+//
+//  _220810__swift_day_48App.swift
+//  [220810] swift_day_48
+//
+//  Created by user on 2022/08/11.
+//
+
+import SwiftUI
+
+@main
+struct _220810__swift_day_48App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
