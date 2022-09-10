685471efd30faa2310950dc69428da1f8502cdef
alpaka99 <ruvan123@naver.com>
2022-09-11 00:29:04 +0900

    [220910] swift_day_66

---
+++ day 61-70/[220910] swift_day_66/[220910] swift_day_66/ImageServer.swift
@@ -0,0 +1,26 @@
+//
+//  ImageServer.swift
+//  [220909] swift_day_65
+//
+//  Created by user on 2022/09/09.
+//
+
+import Foundation
+import UIKit
+
+class ImageServer: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
---
+++ day 61-70/[220910] swift_day_66/[220910] swift_day_66/_220910__swift_day_66App.swift
@@ -0,0 +1,17 @@
+//
+//  _220910__swift_day_66App.swift
+//  [220910] swift_day_66
+//
+//  Created by user on 2022/09/10.
+//
+
+import SwiftUI
+
+@main
+struct _220910__swift_day_66App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
