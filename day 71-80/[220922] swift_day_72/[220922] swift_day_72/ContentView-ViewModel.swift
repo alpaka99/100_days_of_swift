b19127f8123198bf27a10fd67209480d81ae604b
alpaka99 <ruvan123@naver.com>
2022-09-22 23:08:44 +0900

    [220922] swift_day_72

---
+++ day 71-80/[220922] swift_day_72/[220922] swift_day_72/ContentView-ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView-ViewModel.swift
+//  [220922] swift_day_72
+//
+//  Created by user on 2022/09/22.
+//
+
+import Foundation
+import MapKit
+
+extension ContentView {
+    @MainActor class ViewModel: ObservableObject {
+        
+    }
+}
---
+++ day 71-80/[220922] swift_day_72/[220922] swift_day_72/_220922__swift_day_72App.swift
@@ -0,0 +1,17 @@
+//
+//  _220922__swift_day_72App.swift
+//  [220922] swift_day_72
+//
+//  Created by user on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct _220922__swift_day_72App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
