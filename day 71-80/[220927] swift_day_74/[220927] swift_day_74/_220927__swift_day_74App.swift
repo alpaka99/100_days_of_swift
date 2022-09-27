b9e3c2966f1c6246727eff8a0064fcc70b51cdea
alpaka99 <ruvan123@naver.com>
2022-09-27 23:46:27 +0900

    [220927] swift_day_74

---
+++ day 71-80/[220927] swift_day_74/[220927] swift_day_74/_220927__swift_day_74App.swift
@@ -0,0 +1,17 @@
+//
+//  _220927__swift_day_74App.swift
+//  [220927] swift_day_74
+//
+//  Created by user on 2022/09/27.
+//
+
+import SwiftUI
+
+@main
+struct _220927__swift_day_74App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
