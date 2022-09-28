3bdd051ce3afe3440ad996d4233a3acab9638eec
alpaka99 <ruvan123@naver.com>
2022-09-28 22:11:32 +0900

    [220928] swift_day_75

---
+++ day 71-80/[220928] swift_day_75/[220928] swift_day_75/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  [220928] swift_day_75
+//
+//  Created by user on 2022/09/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day 71-80/[220928] swift_day_75/[220928] swift_day_75/_220928__swift_day_75App.swift
@@ -0,0 +1,17 @@
+//
+//  _220928__swift_day_75App.swift
+//  [220928] swift_day_75
+//
+//  Created by user on 2022/09/28.
+//
+
+import SwiftUI
+
+@main
+struct _220928__swift_day_75App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
